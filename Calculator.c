7a5c9b584c1308568c246ded7de3213ad8aea459
Sakthivel <95169262+me-sakthivel@users.noreply.github.com>
2022-01-14 13:29:40 +0530

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void main ()
+{
+     float a,b;
+     int c;
+      printf("------------NOTE----------- \n");
+      printf("You can able to do one opration at a time \n");
+      printf("Enter the two numbers\n");
+      scanf("%f" , &a);
+      scanf("%f" , &b);
+      scanf("%d" , &c);
+
+      switch (c)
+      { 
+          case 1:
+      {
+          printf("%f",(a+b));
+          break;
+      }
+      case 2:
+      {
+          printf("%f", (a-b));
+          break;
+      }
+      case 3:
+      {
+          printf("%f", (a*b));
+          break;
+      }
+      case 4:
+      {
+          printf("%f",(a/b));
+          break;
+      }
+      
+      default:
+      {
+          printf("Invalid credentials");
+        break;
+      }
+
+    }
+}
