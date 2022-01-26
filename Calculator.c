e46dba58ac18246734a76104f8c2335e68075d97
Sakthivel <95169262+me-sakthivel@users.noreply.github.com>
2022-01-26 15:45:54 +0530

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,44 +1,43 @@
 #include <stdio.h>
-#include <conio.h>
-int main()
+#include <conio.h> 
+int main ()
 {
-     float a,b;
-     int c;
-      printf("------------NOTE----------- \n"You can able to do one opration at a time\n Eter the two numbers\n");
-      scanf("%f%f%d,a&,&b,&c);
+    float a,b;
+    int c;
+    printf("Enter the two numbers \n Enter the num A&B");
+    scanf("%f%f%d",&a,&b,&c);\
 
-      switch (c)
-      { 
-          case (1):
-      {
-          printf("%f",(a+b));
-          break;
-      }
-      case (2):
-      {
-          printf("%f", (a-b));
-          break;
-      }
-      case (3):
-      {
-          printf("%f", (a*b));
-          break;
-      }
-      case (4):
-      {
-          printf("%f",(a/b));
-          break;
-      }
-      
-      default:
-      {
-          printf("Invalid credentials");
-        break;
-      }
+    switch (c)
+    {
+        case 1:
+        {
+            printf("%f",a+b);
+            break;
+        }
 
+        case 2:
+        {
+            printf("%f",a*b);
+            break;
+        }
+
+        case 3:
+        {
+            printf("%f",a*b);
+            break;
+        }
+
+        case 4:
+        {
+            printf("%f",a/b);
+            break;
+        }
+
+        default :
+        {
+            printf("Invalid credentials");
+            break;
+        }
     }
-    getch();
     return 0;
-    
-   }
-            
+}
